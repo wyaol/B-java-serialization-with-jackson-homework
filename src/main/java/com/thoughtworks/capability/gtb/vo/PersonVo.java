05bb41c19b0bbb1002226eb10d952f8248c0b9f3
wyao <289672494@qq.com>
2020-11-05 19:00:04 +0800

    [wang yao] feature: 完成问题2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,8 +1,16 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.IOException;
 
 @Data
 @NoArgsConstructor
@@ -10,7 +18,17 @@ import lombok.NoArgsConstructor;
 public class PersonVo {
 
   private String id;
+  @JsonSerialize(using = CustomIntegerSerialize.class)
   private Integer age;
   private String name;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
+}
+
+class CustomIntegerSerialize extends JsonSerializer<Integer> {
+  @Override
+  public void serialize(Integer value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+    if (value == null) gen.writeNumber(0);
+    else gen.writeNumber((Integer) value);
+  }
 }
