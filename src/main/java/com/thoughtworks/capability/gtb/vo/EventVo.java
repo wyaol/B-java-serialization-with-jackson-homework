ebc55a9caa58ea3e9481e6d430f3b4cfa0b64730
wyao <289672494@qq.com>
2020-11-05 18:22:01 +0800

    [wang yao] feature: 完成问题1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,14 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import java.io.IOException;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +21,20 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using = CustomDateSerialize.class)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
+}
+
+class CustomDateSerialize extends StdSerializer<Date> {
+
+  protected CustomDateSerialize() {
+    super(Date.class);
+  }
+
+  @Override
+  public void serialize(Date date, JsonGenerator gen, SerializerProvider provider) throws IOException {
+    gen.writeNumber(date.getTime());
+  }
 }
